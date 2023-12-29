15138a744b1e52cede6c2d4ea297de217063867e
Marco Pausini <marco.pausini@ast-science.com>
2023-12-29 16:42:20 +0000

    [dev] Refactor decimator data types for clarity
    Replaced generic complex data types with fixed-point arithmetic types for input, intermediate, and output data, better reflecting the decimation process's precision requirements. Shifted from streams to plain data structures to simplify interface and improve integration with other system components.

--- hw/src/ssr_multistage_decimator.h
+++ hw/src/ssr_multistage_decimator.h
@@ -13,42 +13,94 @@
 #define SSR_MULTISTAGE_DECIMATOR_H_
 
 
-#include <hls_stream.h>
 #include "ap_fixed.h"
 #include "ap_int.h"
 
-#include "hls_fir.h"
+// C++ class (ap_shift_reg) to ensure that the shift register defined in the C code is always implemented using an SRL resource
+#include "ap_shift_reg.h"
 
-using namespace std;
 
 // decimation factor data type:
 typedef ap_uint<8> dec_factor_t;
 
-// input/output data types: complex s16.15
-constexpr int num_bits = 16;
-constexpr int frac_bits = 15;
-typedef ap_fixed<num_bits, num_bits - frac_bits> fixed_point_t;
-// define template for array of complex data type
-template <size_t N>
-struct cdata_t {
-    fixed_point_t re[N]; // Array of N elements for the real part
-    fixed_point_t im[N]; // Array of N elements for the imaginary part
+// fixed point data type:
+//constexpr unsigned int num_coefficients = 31;
+constexpr int coef_bits = 18;
+constexpr int coef_fractional_bits = 17;
+constexpr int coef_integer_bits = coef_bits - coef_fractional_bits;
+//
+constexpr int datain_bits = 16;
+constexpr int datain_fractional_bits = 15;
+constexpr int datain_integer_bits = datain_bits - datain_fractional_bits;
+//
+constexpr int data_bits = 16;
+constexpr int data_fractional_bits = 15;
+constexpr int data_integer_bits = data_bits - data_fractional_bits;
+//
+constexpr int dataout_bits = 16;
+constexpr int dataout_fractional_bits = 15;
+constexpr int dataout_integer_bits = dataout_bits - dataout_fractional_bits;
+
+typedef ap_int<coef_bits> coef_int_t;                                               // integer type to load coefficients from file
+typedef ap_fixed<coef_bits, coef_integer_bits> coef_t;                              // coef is s18.17
+typedef ap_fixed<datain_bits, datain_integer_bits> datain_t;                        // data is s16.15
+typedef ap_fixed<data_bits, data_integer_bits> data_t;                              // data is s16.15
+typedef ap_fixed<34, 34 - 32> mult_t;                                   // s18.17 x s16.15 = s34.32
+typedef ap_fixed<40, 40 - 32> acc_t;                                    // 
+typedef ap_fixed<dataout_bits, dataout_integer_bits, AP_RND_INF, AP_SAT> dataout_t; // s16.15
+typedef ap_uint<8> coef_addr_t;
+
+// Define a complex number struct
+typedef struct
+{
+    datain_t re;
+    datain_t im;
+} cdatain_t;
+
+typedef struct
+{
+    dataout_t re;
+    dataout_t im;
+} cdataout_t;
+
+typedef struct 
+{
+    data_t re;
+    data_t im;
+} cdata_t;
+
+typedef struct
+{
+    acc_t re;
+    acc_t im;
+} cacc_t;
+
+template <std::size_t N>
+struct cdatain_vec_t
+{
+    datain_t re[N];
+    datain_t im[N];
 };
 
+template <std::size_t N>
+struct cdata_vec_t
+{
+    data_t re[N];
+    data_t im[N];
+};
+
+template <std::size_t N>
+struct cdataout_vec_t
+{
+    dataout_t re[N];
+    dataout_t im[N];
+};
+
+
 // Super-Sample Rate => Parallelism Factor - or Hardware Oversampling Rate
-const size_t ssr = 8;
+const std::size_t ssr = 8;
 
 // top level function
-void ssr_multistage_decimator(hls::stream<cdata_t<ssr>> &in, hls::stream<cdata_t<ssr>> &out, dec_factor_t dec_factor);
-
-// from 1280_to_640 (decimation factor = 2)
-//void ssr_dec2(hls::stream<data_vec8_t> &in, hls::stream<data_vec4_t> &out);
-void hbf_ssr8(hls::stream<cdata_t<ssr>> &in, hls::stream<cdata_t<ssr/2>> &out);
-// from 640_to_320 (decimation factor = 4)
-void hbf_ssr4(hls::stream<cdata_t<ssr/2>> &in, hls::stream<cdata_t<ssr/4>> &out);
-// from 320_to_160 (decimation factor = 8)
-void hbf_ssr2(hls::stream<cdata_t<ssr/4>> &in, hls::stream<cdata_t<ssr/8>> &out);
-// from 160_to_80 + from 80_to_40 + from 40_to_20 (decimation factor = 16,32,64)
-void hbf_ssr1(hls::stream<cdata_t<1>> &in, hls::stream<cdata_t<1>> &out);
+void ssr_multistage_decimator(dec_factor_t dec_factor, bool tvalid_i, cdatain_vec_t<ssr> tdata_i, bool &tvalid_o, cdataout_vec_t<ssr> &tdata_o);
 
 #endif // SSR_MULTISTAGE_DECIMATOR
